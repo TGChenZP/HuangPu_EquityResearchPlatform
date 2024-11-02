c4baea2328b430156f0d7128f2afde79217755a3
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2024-11-03 00:04:38 +1100

    update: finish pdf (include fundementals)

--- notebook/utils/analyse_fundementals.py
+++ notebook/utils/analyse_fundementals.py
@@ -48,10 +48,10 @@ def get_cashflow_df(object: yf.Ticker, TICKER: str) -> pd.DataFrame:
     return cashflow_df
 
 
-def get_financials_df(object: yf.Ticker, TICKER: str) -> pd.DataFrame:
-    financial_df_list = list()
+def get_fundemental_cols_dfs(object: yf.Ticker, TICKER: str) -> pd.DataFrame:
+    fundementals_df_list = list()
 
-    financial_rows = [
+    fundementals_rows = [
         "EBITDA",
         "EBIT",
         "Gross Profit",  # profit
@@ -61,16 +61,17 @@ def get_financials_df(object: yf.Ticker, TICKER: str) -> pd.DataFrame:
         "Interest Expense",  # interest expense
     ]
 
-    for financial_row in financial_rows:
+    for fundementals_row in fundementals_rows:
         try:
-            finacial_row = object.financials.loc[financial_row]
-            financial_df_list.append(finacial_row)
+            finacial_row = object.financials.loc[fundementals_row]
+            fundementals_df_list.append(finacial_row)
         except KeyError as e:
-            print(f"Missing data for {financial_row} for ticker {TICKER}: {e}")
+            print(
+                f"Missing data for {fundementals_row} for ticker {TICKER}: {e}")
 
-    financials_df = pd.concat(financial_df_list, axis=1)
+    fundementals_df = pd.concat(fundementals_df_list, axis=1)
 
-    return financials_df
+    return fundementals_df
 
 
 def create_compound_key_features(
@@ -234,11 +235,12 @@ def get_key_statistics(stock_fundementals: pd.DataFrame, ticker: str) -> pd.Data
     return stock_fundementals
 
 
-def get_key_interested_stat_pct_change(
-    key_interested_stats: pd.DataFrame,
+def get_key_interested_fundementals_stat_pct_change(
+    key_interested_fundementals_stats: pd.DataFrame,
 ) -> pd.DataFrame:
-    key_interested_stats_pct_change = key_interested_stats.pct_change() * 100
-    key_interested_stats_pct_change.rename(
+    key_interested_fundementals_stats_pct_change = key_interested_fundementals_stats.pct_change() * \
+        100
+    key_interested_fundementals_stats_pct_change.rename(
         columns={
             "Net Profit Margin": "Net Profit Margin (%)",
             "Net Income Margin": "Net Income Margin (%)",
@@ -253,7 +255,7 @@ def get_key_interested_stat_pct_change(
         },
         inplace=True,
     )
-    return key_interested_stats_pct_change
+    return key_interested_fundementals_stats_pct_change.astype(float).round(2)
 
 
 def preprocess_dividends(
@@ -322,34 +324,36 @@ def get_other_features(
     return stock_fundementals
 
 
-def get_fundemental_dfs(first_end_of_quarter, historical_prices, TICKER):
+def get_fundementals_dfs(first_end_of_quarter, historical_prices, TICKER):
 
     object = yf.Ticker(f"{TICKER}.AX")
 
     # get balance sheet, concat, sort and change index to date
     balance_sheet_df = get_balance_sheet_df(object, TICKER)
     cashflow_df = get_cashflow_df(object, TICKER)
-    financials_df = get_financials_df(object, TICKER)
+    fundementals_df = get_fundemental_cols_dfs(object, TICKER)
 
     stock_fundementals = pd.concat(
-        [balance_sheet_df, cashflow_df, financials_df], axis=1
+        [balance_sheet_df, cashflow_df, fundementals_df], axis=1
     )
 
     stock_fundementals = process_stock_fundementals(
         stock_fundementals, object, first_end_of_quarter, historical_prices, TICKER)
 
-    raw_stats = get_raw_stats(stock_fundementals)
+    raw_fundementals_stats = get_raw_stats(stock_fundementals)
 
-    key_interested_stats = get_key_interested_stats(stock_fundementals)
+    key_interested_fundementals_stats = get_key_interested_fundementals_stats(
+        stock_fundementals)
 
-    key_interested_stats_pct_change = get_key_interested_stat_pct_change(
-        key_interested_stats
+    key_interested_fundementals_stats_fundementals_pct_change = get_key_interested_fundementals_stat_pct_change(
+        key_interested_fundementals_stats
     )
 
     return (
-        raw_stats,
-        key_interested_stats.astype(float).round(2),
-        key_interested_stats_pct_change.astype(float).round(2),
+        raw_fundementals_stats,
+        key_interested_fundementals_stats.astype(float).round(2),
+        key_interested_fundementals_stats_fundementals_pct_change.astype(
+            float).round(2),
         object
     )
 
@@ -388,8 +392,8 @@ def get_raw_stats(stock_fundementals: pd.DataFrame) -> pd.DataFrame:
     return raw_stats
 
 
-def get_key_interested_stats(stock_fundementals: pd.DataFrame) -> pd.DataFrame:
-    key_interested_stats = stock_fundementals[
+def get_key_interested_fundementals_stats(stock_fundementals: pd.DataFrame) -> pd.DataFrame:
+    key_interested_fundementals_stats = stock_fundementals[
         [
             "Net Profit Margin",
             "Net Income Margin",
@@ -403,10 +407,10 @@ def get_key_interested_stats(stock_fundementals: pd.DataFrame) -> pd.DataFrame:
             "DPS",
         ]
     ]
-    return key_interested_stats
+    return key_interested_fundementals_stats
 
 
-def interpolate_financial_stats(raw_stats_list: list, key: str) -> pd.DataFrame:
+def interpolate_fundementals_stats(raw_stats_list: list, key: str) -> pd.DataFrame:
 
     raw_stats_list[key].index = [pd.to_datetime(dt.strftime('%Y-%m'))
                                  for dt in raw_stats_list[key].index]
@@ -443,31 +447,32 @@ def interpolate_financial_stats(raw_stats_list: list, key: str) -> pd.DataFrame:
     return out
 
 
-def agg_interpolated_financial_stats(interpolated_financial_stats: pd.DataFrame, interested_dates: list) -> pd.DataFrame:
+def agg_interpolated_fundementals_stats(interpolated_fundementals_stats: pd.DataFrame, interested_dates: list) -> pd.DataFrame:
 
     new_rows_dict = dict()
 
     for i, dt in enumerate(interested_dates):
         if i == 0:
-            interested_rows = interpolated_financial_stats[interpolated_financial_stats.index <= dt]
+            interested_rows = interpolated_fundementals_stats[
+                interpolated_fundementals_stats.index <= dt]
         else:
-            interested_rows = interpolated_financial_stats[(interpolated_financial_stats.index <= dt) & (
-                interpolated_financial_stats.index > interested_dates[i-1])]
+            interested_rows = interpolated_fundementals_stats[(interpolated_fundementals_stats.index <= dt) & (
+                interpolated_fundementals_stats.index > interested_dates[i-1])]
 
         interested_rows = interested_rows.sum()
 
         new_rows_dict[dt] = interested_rows
 
-    interested_financial_stats = pd.DataFrame(
-        new_rows_dict, index=interpolated_financial_stats.columns).T
+    interested_fundementals_stats = pd.DataFrame(
+        new_rows_dict, index=interpolated_fundementals_stats.columns).T
 
-    return interested_financial_stats
+    return interested_fundementals_stats
 
 
-def get_weighted_financials(key_interested_stats_pct_change_dict: dict, same_gics_industry_weight_dict: dict) -> pd.DataFrame:
-    """ Get the weighted financials of the key interested stats """
+def get_weighted_fundementals(key_interested_fundementals_stats_pct_change_dict: dict, same_gics_industry_weight_dict: dict) -> pd.DataFrame:
+    """ Get the weighted fundementals of the key interested stats """
 
-    example_df = key_interested_stats_pct_change_dict[list(key_interested_stats_pct_change_dict.keys())[
+    example_df = key_interested_fundementals_stats_pct_change_dict[list(key_interested_fundementals_stats_pct_change_dict.keys())[
         0]]
 
     output_dict = dict()
@@ -480,9 +485,9 @@ def get_weighted_financials(key_interested_stats_pct_change_dict: dict, same_gic
 
             weight = 0
 
-            for key in key_interested_stats_pct_change_dict:
+            for key in key_interested_fundementals_stats_pct_change_dict:
 
-                data = key_interested_stats_pct_change_dict[key].loc[date, column]
+                data = key_interested_fundementals_stats_pct_change_dict[key].loc[date, column]
 
                 if not pd.isna(data) and data != np.inf:
                     output_dict[date][column] += data * \
@@ -491,7 +496,146 @@ def get_weighted_financials(key_interested_stats_pct_change_dict: dict, same_gic
 
             if weight != 0:
                 output_dict[date][column] /= weight
-        weighted_GICS_key_interested_stats_pct_change = pd.DataFrame(
+        weighted_GICS_key_interested_fundementals_stats_pct_change = pd.DataFrame(
             output_dict)
-        weighted_GICS_key_interested_stats_pct_change = weighted_GICS_key_interested_stats_pct_change.T
-    return weighted_GICS_key_interested_stats_pct_change.astype(float).round(2)
+        weighted_GICS_key_interested_fundementals_stats_pct_change = weighted_GICS_key_interested_fundementals_stats_pct_change.T
+    return weighted_GICS_key_interested_fundementals_stats_pct_change
+
+
+def plot_raw_fundementals_stats_table(interested_ticker_raw_fundementals_stats: pd.DataFrame, TICKER: str):
+    # Create the plot for the table
+    fig, ax = plt.subplots(figsize=(14, 6))  # Adjust the figure size as needed
+    ax.axis('tight')
+    ax.axis('off')
+
+    # Create a table in the plot
+    table = ax.table(cellText=interested_ticker_raw_fundementals_stats.T.values,
+                     colLabels=interested_ticker_raw_fundementals_stats.T.columns,
+                     rowLabels=interested_ticker_raw_fundementals_stats.T.index,
+                     cellLoc='center', loc='center')
+
+    # Bold the header row
+    for key, cell in table.get_celld().items():
+        if key[0] == 0:  # This selects the header row
+            cell.set_text_props(fontweight='bold')
+
+    # Set the title with bold font
+    plt.title(f"{TICKER} Raw Stats", fontsize=16, fontweight='bold')
+
+    # Save the plot as an image
+    plt.savefig(
+        f'../outputs/{TICKER}_interested_ticker_raw_stats.png', bbox_inches='tight', dpi=300)
+
+    # Optionally display the plot
+    plt.show()
+
+
+def plot_key_fundementals_multipliers_table(interested_ticker_key_interested_fundementals_stats: pd.DataFrame, TICKER: str):
+    # Create the plot for the table
+    fig, ax = plt.subplots(figsize=(14, 3))  # Adjust the figure size as needed
+    ax.axis('tight')
+    ax.axis('off')
+
+    # Create a table in the plot
+    table = ax.table(cellText=interested_ticker_key_interested_fundementals_stats.T.values,
+                     colLabels=interested_ticker_key_interested_fundementals_stats.T.columns,
+                     rowLabels=interested_ticker_key_interested_fundementals_stats.T.index,
+                     cellLoc='center', loc='center')
+
+    # Bold the header row
+    for key, cell in table.get_celld().items():
+        if key[0] == 0:  # This selects the header row
+            cell.set_text_props(fontweight='bold')
+
+    # Set the title with bold font
+    plt.title(f"{TICKER} Key fundementals Multipliers",
+              fontsize=16, fontweight='bold')
+
+    # Save the plot as an image
+    plt.savefig(
+        f'../outputs/{TICKER}_interested_ticker_key_interested_stats.png', bbox_inches='tight', dpi=300)
+
+    # Optionally display the plot
+    plt.show()
+
+
+def plot_key_fundementals_multipliers_pct_change_table(interested_ticker_key_interested_fundementals_stats_pct_change: pd.DataFrame, TICKER: str):
+    # Create the plot for the table
+    fig, ax = plt.subplots(figsize=(14, 3))  # Adjust the figure size as needed
+    ax.axis('tight')
+    ax.axis('off')
+
+    # Create a table in the plot
+    table = ax.table(cellText=interested_ticker_key_interested_fundementals_stats_pct_change.T.values,
+                     colLabels=interested_ticker_key_interested_fundementals_stats_pct_change.T.columns,
+                     rowLabels=interested_ticker_key_interested_fundementals_stats_pct_change.T.index,
+                     cellLoc='center', loc='center')
+
+    # Bold the header row
+    for key, cell in table.get_celld().items():
+        if key[0] == 0:  # This selects the header row
+            cell.set_text_props(fontweight='bold')
+
+    # Set the title with bold font
+    plt.title(f"{TICKER} Key Stats %Change", fontsize=16, fontweight='bold')
+
+    # Save the plot as an image
+    plt.savefig(
+        f'../outputs/{TICKER}_interested_ticker_key_interested_stats_pct_change.png', bbox_inches='tight', dpi=300)
+
+    # Optionally display the plot
+    plt.show()
+
+
+def plot_key_fundamentals_multipliers(interested_ticker_key_interested_fundamentals_stats: pd.DataFrame,
+                                      weighted_GICS_key_interested_stats: pd.DataFrame,
+                                      TICKER: str,
+                                      compared_type: str):
+    for column in interested_ticker_key_interested_fundamentals_stats.columns:
+        fig, ax = plt.subplots(figsize=(6, 4))
+
+        primary_data = None
+        gics_data = None
+
+        # Plot the primary ticker data if available
+        try:
+            primary_data = interested_ticker_key_interested_fundamentals_stats[column]
+            ax.plot(interested_ticker_key_interested_fundamentals_stats.index,
+                    primary_data, label=TICKER)
+
+            for i in range(1, len(primary_data)):
+                pct_change_primary = (
+                    (primary_data[i] - primary_data[i - 1]) / primary_data[i - 1]) * 100
+                ax.text(primary_data.index[i], primary_data[i],
+                        f'{pct_change_primary:.1f}%', color='black', fontsize=8, ha='center')
+        except KeyError as e:
+            print(f"Column {column} not found for ticker {TICKER}: {e}")
+
+        # Plot the weighted GICS data if available
+        try:
+            label_suffix = 'GICS I.WMean' if 'industry' in compared_type else 'GICS S.WMean'
+            gics_data = weighted_GICS_key_interested_stats[column]
+            ax.plot(weighted_GICS_key_interested_stats.index,
+                    gics_data, label=f'{TICKER} {label_suffix}')
+
+            for i in range(1, len(gics_data)):
+                pct_change_gics = (
+                    (gics_data[i] - gics_data[i - 1]) / gics_data[i - 1]) * 100
+                ax.text(gics_data.index[i], gics_data[i],
+                        f'{pct_change_gics:.1f}%', color='gray', fontsize=8, ha='center')
+        except KeyError as e:
+            print(f"Column {column} not found for GICS: {e}")
+
+        # Set labels and title
+        ax.set_xlabel('Date')
+        ax.set_ylabel(column)
+        plt.title(f'{TICKER} {column} Comparison', fontsize=16)
+
+        plt.xticks(rotation=45)
+        plt.legend()
+
+        # Replace any '/' in the column name to save the file properly
+        safe_column_name = column.replace('/', '_')
+        plt.savefig(
+            f'../outputs/{TICKER}_{safe_column_name}_comparison.png', bbox_inches='tight', dpi=300)
+        plt.show()
