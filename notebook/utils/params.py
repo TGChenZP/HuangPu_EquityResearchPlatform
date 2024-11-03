e2c9018e93d86e3f0670ed4197e98b906edbe8d7
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2024-11-03 15:48:00 +1100

    add US tickers

--- notebook/utils/params.py
+++ notebook/utils/params.py
@@ -1,5 +1,5 @@
-# Proxy for 200, 300, small ord, all ords, govt bond
-index_tickers_list = ['^AXJO', '^AXKO', '^AXSO', '^AORD']
+# Proxy for 200, 300, small ord, all ords
+
 AU_GICS_FILL = {
     'NWSLV.AX': {'Sector': 'Communication Services', 'Industry': 'Entertainment'},
     'KKC.AX': {'Sector': 'Financials', 'Industry': 'Diversified Financials'},
@@ -9,3 +9,6 @@ AU_GICS_FILL = {
     'VG1.AX': {'Sector': 'Financials', 'Industry': 'Investment Management'},
     'WQG.AX': {'Sector': 'Financials', 'Industry': 'Investment Management'}
 }
+
+au_index_tickers_list = ['^AXJO', '^AXKO', '^AXSO', '^AORD']
+us_index_tickers_list = ['^GSPC', '^RUA', '^RUT', '^W5000']
