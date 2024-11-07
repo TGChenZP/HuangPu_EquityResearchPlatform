49e030849db1fa3259c6fe240c8856fbbb3caf1f
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2024-11-08 10:46:28 +1100

    new upgrade: columns

--- notebook/utils/params.py
+++ notebook/utils/params.py
@@ -12,3 +12,42 @@ AU_GICS_FILL = {
 
 au_index_tickers_list = ['^AXJO', '^AXKO', '^AXSO', '^AORD']
 us_index_tickers_list = ['^GSPC', '^RUA', '^RUT', '^W5000']
+
+
+FUNDEMENTALS_RAW_COLUMNS = [
+    "Net Profit Margin",
+    "Net Income Margin",
+    "RoE",
+    "RoA",
+    "P/E",
+    "P/B",
+    "D/E",
+    "Current Ratio",
+    "Interest Coverage Ratio",
+    "DPS",
+    "Free Cash Flow",
+]
+
+CASHFLOW_ROWS = [
+    "Free Cash Flow",  # cashflow
+    "Interest Paid Supplemental Data",
+    "Income Tax Paid Supplemental Data",
+]
+
+BALANCE_SHEET_ROWS = [
+    "Total Debt",  # total debt
+    "Stockholders Equity",  # total shareholder equity.
+    "Share Issued",  # n shares
+    "Current Liabilities",  # total current liabilities,
+    "Current Assets",  # total current assets
+]
+
+FUNDEMENTAL_ROWS = [
+    "EBITDA",
+    "EBIT",
+    "Gross Profit",  # profit
+    "Operating Expense",
+    "Net Income",  # net income
+    "Total Revenue",  # revenue
+    "Interest Expense",  # interest expense
+]
